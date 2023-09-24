08f5099e62c3e79141a553189fef918c2a7689fd
EmanuelBacalhau <carlosemanuelbatistabacalhau@gmail.com>
2023-09-24 20:11:25 -0300

    feat: Send data of register for backend #9

--- src/contexts/signOut.ts
+++ src/contexts/signOut.ts
@@ -2,12 +2,8 @@ import { destroyCookie } from 'nookies'
 import { useRouter } from 'next/navigation'
 
 export function useSignOut(context = undefined) {
-  try {
-    const router = useRouter()
-    const SECRET = process.env.NEXT_PUBLIC_SECRET
-    destroyCookie(context, SECRET as string)
-    router.push('/')
-  } catch (error) {
-    console.log('Erro ao deslogar')
-  }
+  const router = useRouter()
+  const SECRET = process.env.NEXT_PUBLIC_SECRET
+  destroyCookie(context, SECRET as string)
+  router.push('/')
 }
