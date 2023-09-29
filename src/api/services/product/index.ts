9d8ef6db16918c166de0dda08322599604881b8c
EmanuelBacalhau <carlosemanuelbatistabacalhau@gmail.com>
2023-09-28 23:51:27 -0300

    feat: adding service for product

---
+++ src/api/services/product/index.ts
@@ -0,0 +1,33 @@
+import { api } from '@/api/api'
+import { AxiosError } from 'axios'
+
+import { cookies } from 'next/headers'
+
+import { toast } from 'react-toastify'
+
+export const productService = {
+  getProducts: async () => {
+    const secret = process.env.NEXT_PUBLIC_SECRET as string
+    const token = cookies().get(secret)?.value
+
+    try {
+      const response = await api.get('/products', {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+
+      return response
+    } catch (error) {
+      if (error instanceof AxiosError) {
+        if (error.response?.status === 401) {
+          toast.error(error.response.data.message)
+        }
+
+        if (error.response?.status === 500) {
+          toast.error('Internal server error')
+        }
+      }
+    }
+  },
+}
